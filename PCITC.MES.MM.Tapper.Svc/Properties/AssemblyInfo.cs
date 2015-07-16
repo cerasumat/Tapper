d88f2856345dedc1c596a23602fb7cce685bf3ef
CeraSumat <jiakai_6499587>
2015-07-16 12:19:38 +0800

    V1.2.0 Update

--- PCITC.MES.MM.Tapper.Svc/Properties/AssemblyInfo.cs
+++ PCITC.MES.MM.Tapper.Svc/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PCITC.MES.MM.Tapper.Svc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tapper Windoes&RESTful Services")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PCITC.MES.MM.Tapper.Svc")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © CeraSumat 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
